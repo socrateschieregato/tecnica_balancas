0f8ca8d5ede6a928966edf056c69f2ecca0ea03c
socrateschieregato <schieregato@gmail.com>
2019-04-21 23:09:41 -0300

    criacao de tabelas parte 1

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from apps.tabelas.models import Pais, Municipio, Estado
 
-# Create your models here.
+
+class Endereco(models.Model):
+    tipo = models.CharField(max_length=10)
+    tipo_log = models.CharField(max_length=10)
+    endereco = models.CharField(max_length=100)
+    numero = models.IntegerField()
+    complemento = models.CharField(30)
+    cep = models.CharField(max_length=9)
+    uf = models.CharField(max_length=2)
+    pais
+    municipio
+    principal = models.BooleanField(default=True)
+
+
+class Telefone(models.Model):
+    tipo = models.CharField(max_length=10)
+    ddd = models.CharField(max_length=2)
+    numero = models.CharField(max_length=10)
+
+
+class Empresa(models.Model):
+    cpf_cnpj = models.CharField('CPF/CNPJ', max_length=14)
+    ie = models.CharField('Inscrição Estadual', max_length=12)
+    nome_razao = models.CharField('Nome/Razão', max_length=100)
+    nome_fantasia = models.CharField('Nome Fantasia', max_length=50)
+    telefone = models.ManyToManyField('Telefone')
+    email = models.CharField(max_length=50)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    im = models.CharField(max_length=8)
+    grupo
+    dt_criacao
+    user
+
+
+
--- apps/tabelas/models.py
+++ apps/tabelas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Pais(models.Model):
+    cod_ibge = models.CharField(max_length=15)
+    pais = models.CharField(max_length=30)
+    sigla = models.CharField(max_length=2)
+
+
+class Municipio(models.Model):
+    cod_ibge = models.CharField(max_length=15)
+    municipio = models.CharField(max_length=30)
+
+
+class Estado(models.Model):
+    cod_ibge = models.CharField(max_length=15)
+    estado = models.CharField(max_length=30)
+    sigla = models.CharField(max_length=2)
+
+
+class Grupo_Empresas(models.Model):
+    descricao = models.CharField(max_length=20)
+
