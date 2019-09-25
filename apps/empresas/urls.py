8282989dd3ba451b49579fcf8222da0b3c6825c6
socrateschieregato <schieregato@gmail.com>
2019-09-24 21:59:37 -0300

    front testes

--- apps/empresas/urls.py
+++ apps/empresas/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from apps.empresas import views
 
 urlpatterns = [
-    path('', views.empresas_home, name='empresas_home'),
-    path('cad_empresa/', views.cad_empresa, name='cad_empresa'),
-    path('upd_empresa/<int:id>', views.upd_empresa, name='upd_empresa'),
-    path('del_empresa/<int:id>', views.del_empresa, name='del_empresa'),
+    # path('', views.empresas_home, name='empresas_home'),
+    path('v1/empresas/', views.EmpresaListCreate.as_view()),
+    # path('cad_empresa/', views.cad_empresa, name='cad_empresa'),
+    # path('upd_empresa/<int:id>', views.upd_empresa, name='upd_empresa'),
+    # path('del_empresa/<int:id>', views.del_empresa, name='del_empresa'),
 ]
--- tecnica_balancas/urls.py
+++ tecnica_balancas/urls.py
@@ -6,9 +6,11 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('apps.home.urls')),
+    path('api/', include('apps.empresas.urls')),
+    path('api/', include('apps.frontend.urls')),
     path('calibracoes/', include('apps.calibracoes.urls')),
     path('tabelas/', include('apps.tabelas.urls')),
-    path('empresas/', include('apps.empresas.urls')),
+    # path('empresas/', include('apps.empresas.urls')),
     path('equipamentos/', include('apps.equipamentos.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
